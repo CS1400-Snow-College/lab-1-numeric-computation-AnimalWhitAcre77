969dbbda2e8ef3860a9aaf82b8efa3f845d79cd5
Ammon Whitaker <AmmWhi48@millardk12.org>
2026-02-04 13:01:45 -0700

    Completed Step 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+This program follows along this tutorial "https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/numbers-in-csharp"
+*/
+
+Console.Clear();
+//WorkWithIntegers();
+
+// 1: explore integer math
+void WorkWithIntegers()
+{
+    int a = 18;
+    int b = 6;
+
+    // addition
+    int c = a + b;
+    Console.WriteLine(c);
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+}
+
+int a = 5;
+int b = 4;
+int c = 2;
+int d = a + b * c;
+Console.WriteLine(d);
